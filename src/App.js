bcac936ae51ff4e3baf4edbb15a15f258cda6350
Etsuko <hi23etsuko03jap@gmail.com>
2024-03-03 01:50:17 +0900

    save

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import { ChatEngine } from 'react-chat-engine';
 
-import LoginForm from './components/LoginForm';
 import ChatFeed from './components/ChatFeed';
-
-
+import LoginForm from './components/LoginForm';
 import './App.css';
 
+const projectID = '500111a7-44f0-4937-81df-c960a767dca7';
 
-
- const App = () => {
+const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
 
-    return (
-      <ChatEngine
-        height="100vh"
-        projectID="500111a7-44f0-4937-81df-c960a767dca7"
-        userName="javascriptmastery"
-        userSecret="123123"
-        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      />
-    );
-  };
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
 
 
- export default App;
+  //500111a7-44f0-4937-81df-c960a767dca7
 
- //saveするための足掻き
+
