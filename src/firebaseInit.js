7ecbc6319e07fe58a9e257e7f40e76e2d9539c33
Etsuko <hi23etsuko03jap@gmail.com>
2024-03-05 18:18:42 +0900

    firebasetetst

--- src/firebaseInit.js
+++ src/firebaseInit.js
@@ -1,10 +1,16 @@
 // firebaseInit.js
 
-import firebase from 'firebase/compat/app';
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
+
+
+
 //import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
-import ChatEngineCore from 'chatengine';
+//import 'firebase/auth';
+//import 'firebase/database';
+//import ChatEngineCore from 'chatengine';
 
 firebase.initializeApp({
     apiKey: "AIzaSyA8l73l4YIAkRGZm8gLgig7B3Cz6-vznyU",
@@ -14,6 +20,7 @@ firebase.initializeApp({
     messagingSenderId: "139460798310",
     appId: "1:139460798310:web:d496ff61842eb28d8d4aa7"
 });
+
 
 
 
