6df8404d69e0080176a576b147019a67f8faccbd
Etsuko <hi23etsuko03jap@gmail.com>
2024-03-06 17:23:30 +0900

    SAVE

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-//import LoginForm from './components/LoginForm';
+import LoginForm from './components/LoginForm';
 import './App.css';
 
 //した二つfirebase関連
