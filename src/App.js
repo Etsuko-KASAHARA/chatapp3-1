50869954a39c238255974bd846e3d80bd4af2cd6
Etsuko <hi23etsuko03jap@gmail.com>
2024-03-06 16:50:46 +0900

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { ChatEngine } from 'react-chat-engine';
 
 import ChatFeed from './components/ChatFeed';
-import LoginForm from './components/LoginForm';
+//import LoginForm from './components/LoginForm';
 import './App.css';
 
 //した二つfirebase関連
@@ -10,20 +10,20 @@ import firebase from './firebaseInit'; // firebaseInit.jsのファイルパス�
 
 
 
-const App = () => {
-  if (!localStorage.getItem('username')) return <LoginForm />;
+//const App = () => {
+  //if (!localStorage.getItem('username')) return <LoginForm />;
 
-  return (
-    <ChatEngine
-      height="100vh"
-      projectID={'500111a7-44f0-4937-81df-c960a767dca7'}
-      userName={localStorage.getItem('username')}
-      userSecret={localStorage.getItem('password')}
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
-      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
-    />
-  );
-};
+  //return (
+   // <ChatEngine
+     // height="100vh"
+     // projectID={'500111a7-44f0-4937-81df-c960a767dca7'}
+      //userName={localStorage.getItem('username')}
+      //userSecret={localStorage.getItem('password')}
+      //renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      //onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    ///>
+  //);
+//};
 
 // infinite scroll, logout, more customizations...
 
